6767a98d7bde34ac81e78c25bf5aeeb3e7dd0fdf
Akash-YS05 <akash.pan05@gmail.com>
2025-10-15 21:34:34 +0530

    ysxrxy

---
+++ Basic Mathematical Probs/divisors.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+
+int div(int n) {
+    
+
+}
+
+int main() {
+
+    int num;
+    bool res;
+    std::cout << "Num: ";
+    std::cin >> num;
+    
+    
+
+}
